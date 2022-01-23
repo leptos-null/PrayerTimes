9e88e79cce561825dd640dd05fd744793306f511
Leptos <leptos.0.null@gmail.com>
2022-01-22 17:34:39 -0800

    [General] Add PrayerTimesKit cross-platform framework

---
+++ PrayerTimesKit/PrayerTimesKit.h
@@ -0,0 +1,18 @@
+//
+//  PrayerTimesKit.h
+//  PrayerTimesKit
+//
+//  Created by Leptos on 1/22/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrayerTimesKit.
+FOUNDATION_EXPORT double PrayerTimesKitVersionNumber;
+
+//! Project version string for PrayerTimesKit.
+FOUNDATION_EXPORT const unsigned char PrayerTimesKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrayerTimesKit/PublicHeader.h>
+
+
