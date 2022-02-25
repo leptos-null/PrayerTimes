ec2270004a35d16e487a0925dc98b0e53f4ccaad
Leptos <leptos.0.null@gmail.com>
2022-02-25 12:20:08 -0800

    [PrayerTimesKit] Add logging to QuiblaManager in guard else blocks

--- PrayerTimesKit/QuiblaManager.swift
+++ PrayerTimesKit/QuiblaManager.swift
@@ -7,13 +7,18 @@
 
 import Foundation
 import CoreLocation
+import os
+
+#if os(iOS) || os(watchOS)
 
 public final class QuiblaManager: ObservableObject {
+    private static let logger = Logger(subsystem: "null.leptos.PrayerTimesKit", category: "QuiblaManager")
+    
     public let locationManager: LocationManager
     public let headingManager: HeadingManager
     
-    @Published public var quiblaCourse: CLLocationDirection?
-    @Published public var quiblaHeading: CLLocationDirection?
+    @Published public private(set) var quiblaCourse: CLLocationDirection?
+    @Published public private(set) var quiblaHeading: CLLocationDirection?
     
     public init(locationManager: LocationManager, headingManager: HeadingManager) {
         self.locationManager = locationManager
@@ -22,7 +27,10 @@ public final class QuiblaManager: ObservableObject {
         locationManager.$location
             .map { location -> CLLocationDirection? in
                 guard let location = location,
-                      location.horizontalAccuracy >= 0 else { return nil }
+                      location.horizontalAccuracy >= 0 else {
+                          Self.logger.notice("location is nil or coordinate is not valid")
+                          return nil
+                      }
                 return location.coordinate.course(to: .kaaba)
             }
             .assign(to: &$quiblaCourse)
@@ -30,11 +38,19 @@ public final class QuiblaManager: ObservableObject {
         headingManager.$heading
             .combineLatest($quiblaCourse) { heading, quiblaCourse -> CLLocationDirection? in
                 guard let heading = heading,
-                      let quiblaCourse = quiblaCourse else { return nil }
+                      let quiblaCourse = quiblaCourse else {
+                          Self.logger.notice("heading or quiblaCourse is nil")
+                          return nil
+                      }
                 let trueHeading = heading.trueHeading
-                guard trueHeading >= 0 else { return nil }
+                guard trueHeading >= 0 else {
+                    Self.logger.notice("trueHeading is not valid")
+                    return nil
+                }
                 return quiblaCourse - trueHeading
             }
             .assign(to: &$quiblaHeading)
     }
 }
+
+#endif
