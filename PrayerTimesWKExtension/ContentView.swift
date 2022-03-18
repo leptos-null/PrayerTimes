7d19fa570c3ff590e2dddbdb1543b5450946d988
Leptos <leptos.0.null@gmail.com>
2022-03-18 11:47:30 -0700

    [PrayerTimesWatch] Add base watchOS app

---
+++ PrayerTimesWKExtension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PrayerTimesWatch WatchKit Extension
+//
+//  Created by Leptos on 3/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PrayerTimesWKExtension/PrayerTimesApp.swift
@@ -0,0 +1,19 @@
+//
+//  PrayerTimesApp.swift
+//  PrayerTimesWatch WatchKit Extension
+//
+//  Created by Leptos on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct PrayerTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
