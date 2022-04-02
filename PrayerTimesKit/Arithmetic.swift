73947ea7d2ecebb5ee340d709ad029e8a09353aa
Leptos <leptos.0.null@gmail.com>
2022-04-02 16:44:26 -0700

    [PrayerTimesKit] Move SolarPosition into its own file
    Fixes bug where equationOfTime may fall outside of the expected range

--- PrayerTimesKit/Arithmetic.swift
+++ PrayerTimesKit/Arithmetic.swift
@@ -40,45 +40,6 @@ extension AngleRadians {
     }
 }
 
-// https://www.astronomycenter.net/pdf/mohamoud_2017.pdf
-struct SolarPosition {
-    /// Latitude of the observer
-    let latitude: AngleRadians
-    /// Declination of the Sun
-    let declination: AngleRadians
-    
-    private let sin_latitude: Double
-    private let sin_declination: Double
-    private let cos_latitude: Double
-    private let cos_declination: Double
-    
-    init(latitude: AngleRadians, declination: AngleRadians) {
-        self.latitude = latitude
-        self.declination = declination
-        
-        self.sin_latitude = sin(latitude)
-        self.sin_declination = sin(declination)
-        self.cos_latitude = cos(latitude)
-        self.cos_declination = cos(declination)
-    }
-    
-    func timeIntervalTo(depressionAngle: AngleRadians) -> TimeInterval {
-        return timeIntervalTo(elevationAngle: -depressionAngle)
-    }
-    
-    func timeIntervalTo(elevationAngle: AngleRadians) -> TimeInterval {
-        let a = sin(elevationAngle) - sin_latitude * sin_declination
-        let b = cos_latitude * cos_declination
-        return acos(a/b) / Arithmetic.radiansInCircle * .day
-    }
-    
-    func timeIntervalTo(shadowFactor: Double) -> TimeInterval {
-        let length = shadowFactor + tan(abs(latitude - declination))
-        let angle = atan(1/length)
-        return timeIntervalTo(elevationAngle: angle)
-    }
-}
-
 extension Date {
     func julianDate() -> Double {
         timeIntervalSinceReferenceDate / .day + 2451910.5
@@ -127,7 +88,13 @@ func solarApproximations(julianDay: JulianDay) -> (declination: AngleRadians, eq
     // "apparent solar time minus mean solar time"
     let equationOfTime: TimeInterval = (meanSolarLng - rightAscension)/Arithmetic.radiansInCircle * .day
     
-    return (declination, equationOfTime)
+    // equationOfTime should approximately be bound to (-0.5, +0.5)
+    // since `constrict(to:)` constricts values to [0, bound)
+    // shift the input by half the range, and then shift the bounded value back
+    let halfDay: TimeInterval = .day/2
+    let constrictedTime = (equationOfTime + halfDay).constrict(to: .day) - halfDay
+    
+    return (declination, constrictedTime)
 }
 
 extension FloatingPoint {
---
+++ PrayerTimesKit/SolarPosition.swift
@@ -0,0 +1,47 @@
+//
+//  SolarPosition.swift
+//  PrayerTimesKit
+//
+//  Created by Leptos on 3/31/22.
+//
+
+import Foundation
+
+// https://www.astronomycenter.net/pdf/mohamoud_2017.pdf
+struct SolarPosition {
+    /// Latitude of the observer
+    let latitude: AngleRadians
+    /// Declination of the Sun
+    let declination: AngleRadians
+    
+    private let sin_latitude: Double
+    private let sin_declination: Double
+    private let cos_latitude: Double
+    private let cos_declination: Double
+    
+    init(latitude: AngleRadians, declination: AngleRadians) {
+        self.latitude = latitude
+        self.declination = declination
+        
+        self.sin_latitude = sin(latitude)
+        self.sin_declination = sin(declination)
+        self.cos_latitude = cos(latitude)
+        self.cos_declination = cos(declination)
+    }
+    
+    func timeIntervalTo(depressionAngle: AngleRadians) -> TimeInterval {
+        return timeIntervalTo(elevationAngle: -depressionAngle)
+    }
+    
+    func timeIntervalTo(elevationAngle: AngleRadians) -> TimeInterval {
+        let a = sin(elevationAngle) - sin_latitude * sin_declination
+        let b = cos_latitude * cos_declination
+        return acos(a/b) / Arithmetic.radiansInCircle * .day
+    }
+    
+    func timeIntervalTo(shadowFactor: Double) -> TimeInterval {
+        let length = shadowFactor + tan(abs(latitude - declination))
+        let angle = atan(1/length)
+        return timeIntervalTo(elevationAngle: angle)
+    }
+}
