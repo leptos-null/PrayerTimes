e8758ad14ba4d780875fb06da2c039cf0457efe7
Leptos <leptos.0.null@gmail.com>
2022-04-03 18:54:20 -0700

    [PrayerTimes] Use the top one-third of LocationPrompt for authorization

--- PrayerTimes/LocationPrompt.swift
+++ PrayerTimes/LocationPrompt.swift
@@ -13,45 +13,49 @@ struct LocationPrompt: View {
     
     var body: some View {
         if locationManager.authorizationStatus != .restricted {
-            VStack {
+            GeometryReader { geometryProxy in
                 VStack {
-                    Spacer()
-                    
-                    Text("Enable Location Access to always see prayer times for your current location.")
-                        .padding()
-                    
-                    Button {
-                        locationManager.requestWhenInUseAuthorization()
-                    } label: {
-                        Label("Enable Location Access", systemImage: "location")
-                            .symbolVariant(.fill)
+                    VStack {
+                        Spacer()
+                        
+                        Text("Enable Location Access to always see prayer times for your current location.")
+                            .padding()
+                        
+                        Button {
+                            locationManager.requestWhenInUseAuthorization()
+                        } label: {
+                            Label("Enable Location Access", systemImage: "location")
+                                .symbolVariant(.fill)
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .hoverEffect()
+                        .disabled(locationManager.authorizationStatus != .notDetermined)
+                        
+                        HStack {
+                            Text("Open Settings to Enable Location Services")
+                            Link(destination: URL(string: "https://support.apple.com/HT207092")!) {
+                                Label("Help", systemImage: "questionmark.circle")
+                                    .labelStyle(.iconOnly)
+                            }
+                            .hoverEffect()
+                        }
+                        .font(.callout)
+                        .opacity((locationManager.authorizationStatus == .denied) ? 1 : 0)
+                        
+                        Spacer()
                     }
-                    .buttonStyle(.borderedProminent)
-                    .hoverEffect()
-                    .disabled(locationManager.authorizationStatus != .notDetermined)
+                    .frame(height: geometryProxy.size.height * 0.32)
                     
                     HStack {
-                        Text("Open Settings to Enable Location Services")
-                        Link(destination: URL(string: "https://support.apple.com/HT207092")!) {
-                            Label("Help", systemImage: "questionmark.circle")
-                                .labelStyle(.iconOnly)
-                        }
-                        .hoverEffect()
+                        Rectangle()
+                            .frame(height: 1)
+                        Text("or")
+                            .font(.callout.smallCaps())
+                        Rectangle()
+                            .frame(height: 1)
                     }
-                    .font(.callout)
-                    .opacity((locationManager.authorizationStatus == .denied) ? 1 : 0)
-                    
-                    Spacer()
+                    OverrideLocationView(locationManager: locationManager)
                 }
-                HStack {
-                    Rectangle()
-                        .frame(height: 1)
-                    Text("or")
-                        .font(.callout.smallCaps())
-                    Rectangle()
-                        .frame(height: 1)
-                }
-                OverrideLocationView(locationManager: locationManager)
             }
         } else {
             OverrideLocationView(locationManager: locationManager)
