77b8e96a9dc7275dcb3cbc1fca808612c7f874a6
Leptos <leptos.0.null@gmail.com>
2024-07-16 14:02:58 -0700

    [PrayerTimes] Remove UIWindowSceneSessionRoleApplication

--- PrayerTimes/AppDelegate.swift
+++ PrayerTimes/AppDelegate.swift
@@ -24,12 +24,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     // MARK: UISceneSession Lifecycle
     
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running,
