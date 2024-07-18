39870b80f82ac18d1724f7634993e181ed260c8a
Leptos <leptos.0.null@gmail.com>
2024-07-18 09:22:09 -0700

    [PrayerTimesApp] Set macOS window size for SCREENSHOT_MODE

--- PrayerTimes/PrayerTimesApp.swift
+++ PrayerTimes/PrayerTimesApp.swift
@@ -12,8 +12,19 @@ struct PrayerTimesApp: App {
     @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     
     var body: some Scene {
+#if SCREENSHOT_MODE && targetEnvironment(macCatalyst)
+        if #available(macCatalyst 17.0, *) {
+            return WindowGroup {
+                ContentView()
+            }
+            .defaultSize(width: 1280, height: 800 - 28) // menu bar height
+        } else {
+            fatalError("defaultSize requires macCatalyst 17.0")
+        }
+#else
         WindowGroup {
             ContentView()
         }
+#endif
     }
 }
