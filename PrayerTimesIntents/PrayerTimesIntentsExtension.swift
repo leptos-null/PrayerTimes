a28e3c9971a7bba67616d01720421ec465ea2dea
Leptos <leptos.0.null@gmail.com>
2024-07-27 11:50:52 -0700

    [PrayerTimesIntents] Add AppShortcutsProvider with phrases

--- PrayerTimesIntents/PrayerTimesIntentsExtension.swift
+++ PrayerTimesIntents/PrayerTimesIntentsExtension.swift
@@ -10,3 +10,26 @@ import AppIntents
 @main
 struct PrayerTimesIntentsExtension: AppIntentsExtension {
 }
+
+struct PrayerTimesShortcutsProvider: AppShortcutsProvider {
+    static var appShortcuts: [AppShortcut] {
+        AppShortcut(
+            intent: PrayerTimeIntent(),
+            phrases: [
+                // a phrase _must_ have `.applicationName` to be recognized.
+                //
+                // my understanding is that each phrase essentially must be able
+                // to be statically matched. in other words, variables that
+                // cannot be enumerated quickly cannot be recognized.
+                // for example, CLPlacemark cannot be part of the phrase because
+                // all of the strings to represent all of the placemarks cannot
+                // be reasonably enumerated.
+                // in my testing, Int and Date also are not matchable.
+                "According to \(.applicationName), what time is \(\.$targetPrayer)?",
+                "According to \(.applicationName), when is \(\.$targetPrayer)?",
+                "According to \(.applicationName), what time is \(\.$targetPrayer) today?",
+                "According to \(.applicationName), when is \(\.$targetPrayer) today?",
+            ]
+        )
+    }
+}
